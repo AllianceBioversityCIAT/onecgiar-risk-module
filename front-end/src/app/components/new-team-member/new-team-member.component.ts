34a5534e53ad53c3b1acecb6f4940ed10492d517
moayadnajd <moayad.najd@gmail.com>
2023-05-24 12:51:10 +0300

    https://codeobia.atlassian.net/browse/RMD-18

--- front-end/src/app/components/new-team-member/new-team-member.component.ts
+++ front-end/src/app/components/new-team-member/new-team-member.component.ts
@@ -17,8 +17,8 @@ export class NewTeamMemberComponent {
 
   Roles: any[] = [
     {value: '', viewValue: 'Role selection'},
-    {value: 'Member', viewValue: 'Member'},
-    {value: 'Leader', viewValue: 'Leader'},
+    {value: 'Lead / Coordinator', viewValue: 'Lead / Coordinator'},
+    {value: 'Team Member', viewValue: 'Team Member'},
     {value: 'Co-leader', viewValue: 'Co-leader'}
   ];
 
