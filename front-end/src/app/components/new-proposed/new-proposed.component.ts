0614cd086402ef7fc58d19148c69524e54ddf73b
tarequi <t.anshasi99@gmail.com>
2023-06-14 14:35:28 +0300

    #48

--- front-end/src/app/components/new-proposed/new-proposed.component.ts
+++ front-end/src/app/components/new-proposed/new-proposed.component.ts
@@ -21,11 +21,11 @@ export class NewProposedComponent {
   ) {}
 
   Actions: any[] = [
-    { value: 'Not-started', viewValue: 'Not started' },
-    { value: 'Delayed', viewValue: 'Delayed' },
-    { value: 'Completed', viewValue: 'Completed' },
-    { value: 'Ongoing and on track', viewValue: 'Ongoing and On Track' },
-    { value: 'Unknown/no visibility', viewValue: 'Unknown/No Visibility' },
+    { value: 'Not-started', viewValue: 'Not started' , toolTip: ''},
+    { value: 'Completed', viewValue: 'Completed', toolTip: ' the action has been completed (could be an one-off action taken or the establishment of a control to manage risk i.e. a standing review introduced or a process has been put in place' },
+    { value: 'Ongoing and on track', viewValue: 'Ongoing and On Track' , toolTip: 'an action is underway, has not been completed but it is considered to be on track according to schedule'},
+    { value: 'Delayed', viewValue: 'Delayed' , toolTip: ' an action is planned or underway, has not been completed but running behind schedule'},
+    { value: 'Unknown/no visibility', viewValue: 'Unknown/No Visibility' , toolTip: ' at the point of the review information on action panned is not available. This may be an action owned by another group but an update is missing (i.e. not known if delayed, ongoing and on-track, completed or not yet started)'},
   ];
 
   proposedForm: any;
