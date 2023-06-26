8281a5b06d44f863aa455ec9872f3dc4f4228d64
moayadnajd <moayad.najd@gmail.com>
2023-06-26 14:08:45 +0300

    socket

--- front-end/src/app/services/socket.service.ts
+++ front-end/src/app/services/socket.service.ts
@@ -1,12 +1,13 @@
 import { Injectable, NgModule } from '@angular/core';
 import { Socket } from 'ngx-socket-io';
+import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class AppSocket extends Socket {
 
     constructor() {
       const token=  localStorage.getItem('access_token');
-        super({ url: 'http://localhost:3000', options: { autoConnect:false, auth:{authorization:'Barer '+token}} });
+        super({ url: environment.backend_url, options: { autoConnect:false, auth:{authorization:'Barer '+token}} });
     }
 
 }
