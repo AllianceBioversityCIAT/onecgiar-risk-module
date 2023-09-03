dbbe6a2d3888a4f938a959eaa502b069c277aa2e
tarequi <t.anshasi99@gmail.com>
2023-09-03 16:30:59 +0300

    add FAQ

--- front-end-new/src/app/Admin/admin-module/faq-admin/faq-admin.component.ts
+++ front-end-new/src/app/Admin/admin-module/faq-admin/faq-admin.component.ts
@@ -1,51 +1,76 @@
 import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { MatTableDataSource } from '@angular/material/table';
-import { ApiGlossaryService } from 'src/app/shared-services/glossary-services/api-glossary.service';
 import { FaqFormDialogComponent } from './faq-form-dialog/faq-form-dialog.component';
-import { ApiFaqService } from 'src/app/shared-services/faq-services/api-faq.service';
-
+import { ToastrService } from 'ngx-toastr';
+import { FAQService } from 'src/app/services/faq.service';
+import { DeleteConfirmDialogComponent } from 'src/app/delete-confirm-dialog/delete-confirm-dialog.component';
 @Component({
   selector: 'app-faq-admin',
   templateUrl: './faq-admin.component.html',
   styleUrls: ['./faq-admin.component.scss'],
 })
 export class FaqAdminComponent {
-  constructor(
-    private apiFaqService: ApiFaqService,
-    private dialog: MatDialog
+  constructor(    
+    private dialog: MatDialog,
+    private toster: ToastrService,
+    private FaqService: FAQService,
   ) {}
 
-  dataSource = new MatTableDataSource(this.apiFaqService.faqData);
 
-  displayedColumns: string[] = ['id', 'question', 'answer', 'actions'];
+  data: any;
 
-  openDialogCreateFaq(title: any) {
-    this.dialog.open(FaqFormDialogComponent, {
+  displayedColumns: string[] = [
+    'id',
+    'question',
+    'answer',
+    'actions'
+  ];
+
+  async ngOnInit() {
+    await this.getData();
+
+  }
+
+
+  async getData() {
+    this.data = await this.FaqService.getData();
+  }
+
+  openFormDialog(id:any, action: string){
+    const _popup = this.dialog.open(FaqFormDialogComponent,{
       width: '68rem',
-      height: '69.2rem',
-      data: {
-        title: title,
-      },
+      height: 'auto',
+      data:{
+        id:id,
+        action: action
+      }
+    });
+    _popup.afterClosed().subscribe(async response => {
+      await this.getData();
     });
   }
 
-  openDialogEditFaq(title: any, element: any) {
-    this.dialog.open(FaqFormDialogComponent, {
-      width: '68rem',
-      height: '69.2rem',
+  editFAQ(id: number, action: string) {
+    this.openFormDialog(id, action);
+  }
 
-      data: {
-        title: title,
-        element: element,
-      },
+
+  deleteFAQ(record: any) {
+    const _popup = this.dialog.open(DeleteConfirmDialogComponent ,{
+      width:'auto',
+      height: 'auto',
+      data:{
+        title:'Delete',
+        message: `Are you sure you want to delete ${record.answer}`
+      }
+    });
+    _popup.afterClosed().subscribe(async response => {
+      if(response == true) {
+        await this.FaqService.deleteFaq(record.id)
+        this.getData();
+        this.toster.success(`Delete ${record.answer} successfully`);
+      }
     });
   }
 
-  deleteFaq(id: any) {
-    this.apiFaqService
-      .openDialogDeleteFaq('Are you sure you want to delete this record ?')
-
-      .afterClosed();
-  }
 }
--- front-end-new/src/app/Admin/admin-module/faq-admin/faq-form-dialog/faq-form-dialog.component.ts
+++ front-end-new/src/app/Admin/admin-module/faq-admin/faq-form-dialog/faq-form-dialog.component.ts
@@ -1,15 +1,11 @@
 import { Component, Inject } from '@angular/core';
 import {
   FormBuilder,
-  FormControl,
-  FormGroup,
   Validators,
 } from '@angular/forms';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
-
-import { Editor, Toolbar } from 'ngx-editor';
+import { MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
+import { FAQService } from 'src/app/services/faq.service';
 import { ToastrService } from 'ngx-toastr';
-import { AnnouncementService } from 'src/app/services/announcement.service';
 
 @Component({
   selector: 'app-faq-form-dialog',
@@ -18,58 +14,57 @@ import { AnnouncementService } from 'src/app/services/announcement.service';
 })
 export class FaqFormDialogComponent {
   dataToEdit: any;
-  setValue: any;
-  constructor(
-    private dialogRef: MatDialogRef<FaqFormDialogComponent>,
-    @Inject(MAT_DIALOG_DATA)
-    public faqData: { title: any; element: any },
-    private toster: ToastrService,
-    private fb: FormBuilder
-  ) {}
-
-  editorQuestion: Editor = new Editor();
-  editorAnswer: Editor = new Editor();
-
-  html: string = '';
-  toolbar: Toolbar = [
-    ['bold', 'italic'],
-    ['underline', 'strike'],
-    ['code', 'blockquote'],
-    ['ordered_list', 'bullet_list'],
-    [{ heading: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'] }],
-    ['link'],
-    ['text_color', 'background_color'],
-    ['align_left', 'align_center', 'align_right', 'align_justify'],
-  ];
-
-  faqForm = this.fb.group({
-    id: this.fb.control({ value: '', disabled: true }),
-    question: this.fb.control('', Validators.required),
-    description: this.fb.control('', [
-      Validators.required,
-      Validators.maxLength(1500),
-    ]),
-    answer: this.fb.control(false, Validators.required),
-  });
-
+  constructor(   
+  @Inject(MAT_DIALOG_DATA) public data:any,
+  private fb: FormBuilder,
+  private dialog: MatDialog,
+  private toster: ToastrService,
+  private FAQService: FAQService,
+  ){}
   ngOnInit(): void {
-    this.editorQuestion = new Editor();
-    this.editorAnswer = new Editor();
     this.setValue();
-    console.log(this.faqData.title);
   }
 
-  // make sure to destory the editor
-  ngOnDestroy(): void {
-    this.editorQuestion.destroy();
-    this.editorAnswer.destroy();
+  Form = this.fb.group({
+    id:this.fb.control({value:'',disabled:true}),
+    question:this.fb.control('',Validators.required),
+    answer:this.fb.control('', [Validators.required]),
+  });
+
+
+  async onSubmit() {
+    const id = this.Form.getRawValue().id;
+    if(id != '' && id != null){
+      if(this.Form.valid){
+        await this.FAQService.editFaq(id ,this.Form.value);
+          this.onCloseDialog();
+          this.toster.success('updated successfully');
+      }
+    }
+    else{
+      if(this.Form.valid){
+        await this.FAQService.createFaq(this.Form.value);
+          this.onCloseDialog();
+          this.toster.success('Added successfully');
+      }
+    }
+  }
+
+
+  async setValue() {
+    if(this.data.id != '' && this.data.id != null){
+      this.dataToEdit = await this.FAQService.getFaqById(this.data.id);
+        this.Form.patchValue({
+          id: this.dataToEdit.id,
+          question: this.dataToEdit.question,
+          answer: this.dataToEdit.answer,
+        });
+    }
   }
 
   //Close-Dialog
   onCloseDialog() {
-    this.dialogRef.close();
+    this.dialog.closeAll();
   }
 
-  onAddFaq() {}
-  onUpdateFaq() {}
 }
