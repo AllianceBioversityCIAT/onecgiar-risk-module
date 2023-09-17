65f0d244096471a92c6a2a2d102186d61168252a
moayadnajd <moayad.najd@gmail.com>
2023-09-17 17:29:29 +0300

    socket.io

--- front-end/src/environments/environment.development.ts
+++ front-end/src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
     backend_url: '/api',
-    socket:"https://risktest.ciat.cgiar.org/api",
+    socket:"https://risktest.ciat.cgiar.org/api/socket.io/",
     aws_cognito_link:'https://ost-toc.auth.us-east-1.amazoncognito.com',
     aws_cognito_client_id:'7j40ekbpnbq4aegcsgjmm66c90',
     aws_cognito_client_redirect_uri:'https://risktest.ciat.cgiar.org/auth'
