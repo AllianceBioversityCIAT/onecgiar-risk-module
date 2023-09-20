1aebc1766facf4a28bbb4f1549495511ed5e7575
mohammad saleem <mohammadsaleem712.cm@gmail.com>
2023-09-20 17:51:45 +0300

    #131

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -5,4 +5,15 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor() {
+    const faviconTag: any = document.getElementById('faviconTag');
+
+    if (
+      window.matchMedia &&
+      window.matchMedia('(prefers-color-scheme: dark)').matches
+    ) {
+      faviconTag.href = '/assets/shared-image/mask-group.svg';
+    } else faviconTag.href = '/assets/shared-image/cgiar-logo.png';
+  }
+}
