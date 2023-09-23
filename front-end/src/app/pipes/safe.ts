fe703da2db44d91ddc1b9734962554fb28cfde1b
mohammad saleem <mohammadsaleem712.cm@gmail.com>
2023-09-23 22:19:21 +0300

    changes for faq about img

--- front-end/src/app/pipes/safe.ts
+++ front-end/src/app/pipes/safe.ts
@@ -1,20 +1,18 @@
-import { Pipe } from "@angular/core";
-import { DomSanitizer } from "@angular/platform-browser";
+import { Pipe } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
-@Pipe({name: 'safeHtml'})
+@Pipe({ name: 'safeHtml' })
 export class Safe {
-  constructor(private sanitizer:DomSanitizer){}
+  constructor(private sanitizer: DomSanitizer) {}
 
-  transform(style:any, title: string) {
-
-    if(title == 'safeHtml for mitigation') {
-      return this.sanitizer.bypassSecurityTrustHtml(style);
-
-    }
-    else {
+  transform(style: any, title: string) {
+    if (title == 'safeHtml for mitigation') {
+      return this.sanitizer.bypassSecurityTrustHtml(
+        `<div class="ww">` + style + `</div>`
+      );
+    } else {
       return style.replace(/<.*?>/g, '');
     }
-
 
     // return this.sanitizer.bypassSecurityTrustHtml(style);
     //return this.sanitizer.bypassSecurityTrustStyle(style);
