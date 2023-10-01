e0c65c8988d04245fec4011a6610c03c953aff3a
moayadnajd <moayad.najd@gmail.com>
2023-10-01 18:55:07 +0300

    env

--- front-end/src/environments/environment.prod.ts
+++ front-end/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  backend_url: 'https://api.risk.cgiar.org',
-  socket:"/socket.io",
+  backend_url: '/api',
+  socket:"/api/socket.io",
   aws_cognito_link:'https://osttoc.auth.us-east-1.amazoncognito.com',
   aws_cognito_client_id:'4mgt0il3e52a2mc52jmmv5hpml',
   aws_cognito_client_redirect_uri:'https://risk.cgiar.org/auth'
